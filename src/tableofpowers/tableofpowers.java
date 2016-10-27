576bfe4a147ac86a26480cc6f2e6cadde74df7c2
Claudia <zavalacl@msu.edu>
2016-10-26 23:54:12 -0400

    Displays results in table format

--- src/tableofpowers/tableofpowers.java
+++ src/tableofpowers/tableofpowers.java
@@ -1,47 +1,60 @@
 package tableofpowers;
+
 import java.util.Scanner;
-import java.lang.Math;
+
 
 public class tableofpowers {
 
 	public static void main(String[] args) {
-		// prompt user for an integer
-				Scanner sc = new Scanner(System.in);
+		/*
+		 * Lab 3 Task: Pair program and display a TABLE OF POWERS. Prompt the
+		 * user to enter an integer. Display a TABLE of SQUARES and CUBES from 1
+		 * to the value entered by user. Ask if the user wants to continue.
+		 * (y/n)? Assume that the user will enter valid data. Only continue if
+		 * the user agrees to.
+		 * 
+		 * @author DanielChristiansen
+		 * 
+		 * @author ClaudiaZavala
+		 */
 
-				// store input in variable
-				
-				String choice = "y";
-				while (choice.equalsIgnoreCase("y")){
-					System.out.println("Please enter an integer: ");
-					int userInput = sc.nextInt();
-					// if input is positive
-					if (userInput >= 1) {
-						// for loop up to int entered by user
-						for (int i = 1; i <= userInput; i++) {
-								// math to calculate square
-								int square = (int) Math.pow(userInput, 2);
-								// math to calculate cube
-								int cubed = (int) Math.pow(userInput, 3);
-								// display results
-								System.out.println("The square of user input is :" + square);
-								System.out.println("The cube of user input is :" + cubed);
-							}
-					}
-					else {
-						System.out.println("Please enter a positive integer: ");
-						// ask to continue
-						sc.nextInt();
-						}
+		System.out.println("Learn your squares and cubes!");
+		System.out.println();
 
-				System.out.print("Would you like to try again? ");
-						choice = sc.next();
-						System.out.println();
-					}
-					sc.close();
+		Scanner sc = new Scanner(System.in);
+		String choice = "y";
 
-				
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.print("Enter an integer: ");
+			int integer = sc.nextInt();
+
+			if (integer >=1 ){
+				String columns = "NUMBER " + "SQUARED " + "CUBED " + "\n" + "====== " + "======= " + "===== ";
+				System.out.println(columns);
+	
+				int square = 0;
+				int cube = 0;
+	
+				String row = "";
+				for (int i = 1; i <= integer; i++) {
+					square = i * i;
+					cube = i * i * i;
+					row = i + "       " + square + "     " + cube;
+					System.out.println(row);
+					System.out.println();
 				}
+			}
+			else{
+				System.out.print("Please enter a positive integer: ");
+				sc.nextInt();
+			}
+		System.out.print("Continue? (y/n): ");
+		choice = sc.next();
+		System.out.println();
 		}
-				
+		sc.close();
+		System.out.println("Goodbye!");
 
+		} 
+}
 
